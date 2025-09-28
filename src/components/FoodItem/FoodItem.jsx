eb33274dd23c7aee3d37c887359166ac9110d28a
Aman Khan <amankassar790@gmail.com>
2025-09-28 12:52:41 +0530

    buil a food delivery app

---
+++ src/components/FoodItem/FoodItem.jsx
@@ -0,0 +1,57 @@
+import React, { useContext } from "react";
+import "./FoodItem.css";
+import { assets } from "../../assets/assets";
+import { StoreContext } from "../../context/StoreContext";
+
+const FoodItem = ({ id, name, price, description, image }) => {
+  const { cartItem, addCartItem, removeCartItem } = useContext(StoreContext);
+
+  return (
+    <div className="food-item">
+      <div className="food-item-img-container">
+        <img className="food-item-img" src={image} alt="" />
+      </div>
+      <div className="food-item-info">
+        <div className="food-item-name-rating">
+          <p>{name}</p>
+          <img src={assets.rating_starts} alt="" />
+        </div>
+        <p className="food-item-disc">{description}</p>
+        <p className="food-item-price">${price}</p>
+        <div className="food-item-buttons-container">
+          {!cartItem[id] ? (
+            <img
+              className="add"
+              onClick={() => {
+                addCartItem(id);
+              }}
+              src={assets.add_icon_white}
+              alt=""
+            />
+          ) : (
+            <div className="food-item-counter">
+              <img
+                className=""
+                onClick={() => {
+                  removeCartItem(id);
+                }}
+                src={assets.remove_icon_red}
+                alt=""
+              />
+              <p>{cartItem[id]}</p>
+              <img
+                onClick={() => {
+                  addCartItem(id);
+                }}
+                src={assets.add_icon_green}
+                alt=""
+              />
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FoodItem;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <div className='header'>
+       <div className="header-content">
+        <h2>Order your favourite food here</h2>
+        <p>Choose from a diverse menu feturing a delectable array of dishes crafted with the finest ingrediants and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experiance, one meal at a time.</p>
+       <button>View menu</button>
+       </div>
+    </div>
+  )
+}
+
+export default Header
